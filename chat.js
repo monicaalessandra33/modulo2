266b6d958b160fb5c16061a63f8c480caebfe57f
monicaalessandra33 <monicaalessandraadm@outlook.com>
2022-02-10 21:20:26 -0300

    inclusão comando else if

--- chat.js
+++ chat.js
@@ -1,11 +1,13 @@
 const input = require ('readline-sync');
 //esta entrada permite que a variável input utilize a dependência readline-sync. O comando require é usado antes da dependência.
-console.log('Seja bem vindo! Precisamos te conhecer melhor');
+console.log('Seja bem vindo(a)! Precisamos te conhecer melhor');
 
-const conhecimento = input.question('Qual o seu conhecimento em informatica? Digite: 0 - Nenhum; 1 - Basico; 2 - Intermediario; 3 - Avancado:');
+const conhecimento = input.question('Qual o seu conhecimento em informatica? Digite: 0-Nenhum; 1-Basico; 2-Intermediario; 3-Avancado:');
 
 if (conhecimento >=1 && conhecimento <= 3) {
-    console.log('Pode participar do curso Back-End')
+    console.log('Parabéns! Você pode participar do curso Back-End!')
+} else if (conhecimento == 0) {
+    console.log('Infelizmente, você precisa ter no mínimo conhecimentos básico em informatica!')
 } else {
-    console.log('Precisa ter conhecimentos básico em informatica')
-};
+    console.log('Digite uma opção válida')
+}
