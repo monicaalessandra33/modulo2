90a8a5a98dbff9f05c9f0dacd0dc18f7da296bd4
monicaalessandra33 <monicaalessandraadm@outlook.com>
2022-02-15 21:17:34 -0300

    Aula Array

---
+++ Exercício_variáveis/array.js
@@ -0,0 +1,33 @@
+// PRIMEIRO EXERCÍCIO
+
+/*
+const lista = new Array('Pera','uva', 'maça')
+
+const primeiroItem = lista[0]
+
+console.log(primeiroItem) */
+
+
+// SEGUNGO EXERCÍCIO
+/*
+const lista = new Array('Pera','uva', 'maça')
+
+const primeiroItem = lista[0]
+
+const tamanho = lista.length
+
+console.log(tamanho)  */
+
+// TERCEIRO EXERCÍCIO
+
+const aluno = [
+    {Nome : 'Janaina' , Curso : 'Back End' , Matrícula: '00210'},
+    {Nome : 'Roberto' , Curso : 'Back End' , Matrícula: '00211'},
+    {Nome : 'Ana' , Curso : 'Back End' , Matrícula: '00212'},
+    {Nome : 'Marcos' , Curso : 'Back End' , Matrícula: '00213'},
+    {Nome : 'Monica' , Curso : 'Back End' , Matrícula: '00214'}
+]
+console.log(aluno)
+// console.log(aluno.length)
+// console.log(aluno[3])
+// console.log('Número da matrícula:' , aluno[3].Matrícula)
