617475ab4847923a9b804b14099a845f75be8eac
monicaalessandra33 <97566896+monicaalessandra33@users.noreply.github.com>
2022-02-22 21:09:22 -0300

    Add files via upload

---
+++ exercicio.js
@@ -0,0 +1,27 @@
+//EXERCÍCIO 1
+
+const input = require ('readline-sync');
+
+let n1 = input.question("Informe a primeira nota: ")
+let n2 = input.question("Informe a segunda nota: ")
+let n3 = input.question("Informe a terceira nota: ")
+let n4 = input.question("Informe a quarta nota: ")
+let n5 = input.question("Informe a quinta nota: ")
+
+console.log("a media é: ",(+n1 + +n2 + +n3 + +n4 + +n5)/5)
+
+
+
+
+//EXERCÍCIO 2
+
+//let frase = ("A TURMA DO BACK END DO SENAC É MASSA");
+
+//console.log(frase+frase.length);
+
+
+//EXERCÍCIO 3
+
+// let fraseDois = ("Monica está desenvolvendo")
+// let fraseInvertida = fraseDois.split("").reverse().join("")
+// console.log("A FRASE É: " + fraseDois + " - " + "A FRASE INVERTIDA É: " + fraseInvertida)
